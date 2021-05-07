e6b7f4332320e8103393f51742cd7c12aab00098
anand siddharth <anand.siddharth@hotmail.com>
2021-05-07 15:38:43 +0530

    fix payment module

--- index.ts
+++ index.ts
@@ -2,7 +2,9 @@ import { module } from './store'
 import { plugin } from './store/plugin'
 import { beforeRegistration } from './hooks/beforeRegistration'
 import { VueStorefrontModule, VueStorefrontModuleConfig } from '@vue-storefront/core/lib/module'
-
+import { StorefrontModule } from 'core/lib/modules'
+import Vue from 'vue';
+import { afterRegistration } from '../google-tag-manager/hooks/afterRegistration'
 export const KEY = 'razorpay'
 
 const moduleConfig: VueStorefrontModuleConfig = {
@@ -12,3 +14,9 @@ const moduleConfig: VueStorefrontModuleConfig = {
 }
 
 export const Razorpay = new VueStorefrontModule(moduleConfig)
+
+export const RazorpayPaymentModule: StorefrontModule = function({ app, store, router, moduleConfig, appConfig }) {
+  beforeRegistration({ Vue, config: appConfig, store, isServer: app.$isServer})
+  store.registerModule(KEY, module)
+  afterRegistration(appConfig, store)
+};
